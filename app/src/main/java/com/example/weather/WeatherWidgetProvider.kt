802e6f3bc8e493660c5216f63d2fc0a24e7a7be7
LAPTOP-P3BPH6JG\yoop1 <yoop1000@naver.com>
2024-01-10 02:03:40 +0900

    feat : 위젯을 provider를 이용해 바탕화면에 생성 후 연결시키기

---
+++ app/src/main/java/com/example/weather/WeatherWidgetProvider.kt
@@ -0,0 +1,53 @@
+package com.example.weather
+
+import android.app.ForegroundServiceStartNotAllowedException
+import android.app.PendingIntent
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.widget.RemoteViews
+import androidx.core.content.ContextCompat
+
+class WeatherAppWidgetProvider : AppWidgetProvider() {
+
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray
+    ) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds)
+
+        appWidgetIds.forEach { appWidgetId ->
+            val pendingIntent: PendingIntent = Intent(context, MainActivity::class.java)
+                .let { intent ->
+                    PendingIntent.getActivity(
+                        context,
+                        1,
+                        intent,
+                        PendingIntent.FLAG_IMMUTABLE
+                    )
+                }
+            val views: RemoteViews = RemoteViews(
+                context.packageName,
+                R.layout.widget_weather
+            ).apply {
+                setOnClickPendingIntent(R.id.widgetTextView, pendingIntent)
+            }
+            appWidgetManager.updateAppWidget(appWidgetId, views)
+        }
+//        val serviceIntent = Intent(context, UpdateWeatherService::class.java)
+//
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+//            try {
+//                ContextCompat.startForegroundService(context, serviceIntent)
+//
+//            } catch (e: ForegroundServiceStartNotAllowedException) {
+//                e.printStackTrace()
+//            }
+//        } else {
+//            ContextCompat.startForegroundService(context, serviceIntent)
+//        }
+    }
+}
